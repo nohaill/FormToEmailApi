8e5b9d47c1924cf4fc4e2d677de57f14bf37453c
nohaill <muhammadnohail1@gmail.com>
2024-10-22 23:15:38 +0500

    Project Files

---
+++ FormToEmailApi/Controllers/ContactFormController.cs
@@ -0,0 +1,50 @@
+﻿using FormToEmailApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace FormToEmailApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactFormController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SubmitForm(
+                [FromQuery] string recipientEmail,
+                [FromBody] ContactForm contactForm)
+        {
+            if (contactForm == null || string.IsNullOrEmpty(recipientEmail) || !ModelState.IsValid)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            try
+            {
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress("muhammadnohail1@gmail.com"); 
+                    mailMessage.To.Add(recipientEmail); 
+                    mailMessage.Subject = "New Contact Form Submission";
+                    mailMessage.Body = $"Name: {contactForm.Name}\n" +
+                                       $"Email: {contactForm.Email}\n" +
+                                       $"Phone: {contactForm.PhoneNumber}\n" +
+                                       $"Message: {contactForm.Message}";
+
+                    using (var smtpClient = new SmtpClient("smtp.gmail.com"))
+                    {
+                        smtpClient.Port = 587; 
+                        smtpClient.Credentials = new System.Net.NetworkCredential("muhammadnohail1@gmail.com", "wjfz ozge hlrg jpcp");
+                        smtpClient.EnableSsl = true;
+                        await smtpClient.SendMailAsync(mailMessage);
+                    }
+                }
+
+                return Ok("Email sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ FormToEmailApi/Models/ContactForm.cs
@@ -0,0 +1,10 @@
+﻿namespace FormToEmailApi.Models
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Message { get; set; }
+    }
+}
